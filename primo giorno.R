0c2b654c6230770d053e4f1c01f71aa9d9481991
fcosta00 <francescosta00@gmail.com>
2023-10-02 12:00:29 +0200

    esercizio introduttivo

---
+++ primo giorno.R
@@ -0,0 +1,23 @@
+E<-3
+g<-sqrt(6^3)
+h<-E-g
+i<-E/g
+J <- log10(50)
+k <- h<0
+
+
+
+# Esercizio 1 
+
+abs(h)>E
+i>=g
+E<=g
+J==2
+J/i!=3/2
+str(k)
+
+abs(h)<E & i!=g
+abs(h)<E | i!=g
+k | FALSE
+(E <= g) & (3^4 > 0 )
+(-h>0)  & (J==2)
