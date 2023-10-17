0deea3d73188efa839e443e12b0fa2219507f2ee
fcosta00 <francescosta00@gmail.com>
2023-10-17 13:25:53 +0200

    Update lez_23-10-16.R

--- lez_23-10-16.R
+++ lez_23-10-16.R
@@ -32,8 +32,7 @@ table(DOCwashing[DOCwashing$Gruppo == 1, c(4,5)])
 #Esercizio 2 - punto 1 -------
 
 library(ggplot2)
-hist_s <- ggplot(DOCwashing, aes(x=Score)) + geom_histogram() #la distribuzione si mostra divisa in due pseudo normali appaiate
-hist_s
+hist_s <- ggplot(DOCwashing, aes(x=Score)) + geom_histogram() #la distribuzione si mostra divisa in due pseudo normali appaiate, per cui bimodale
 
 hist_s <- ggplot(DOCwashing, aes(x=Score)) + geom_histogram(binwidth=0.5)
 
@@ -41,7 +40,7 @@ hist_s <- hist_s +
           theme(axis.text.x = element_text(color="red"),  # Cambia colore etichette asse x
                 axis.text.y = element_text(color="blue"))
 hist_s <- hist_s +
-          scale_x_continuous(breaks = seq(0, 20, by = 2)) +
+          scale_x_continuous(breaks = seq(0, 20, by = 2)) + #cambio i ticks e la frequenza degli assi
           scale_y_continuous(breaks = seq(0, 20, by = 2)) 
 hist_s
 
