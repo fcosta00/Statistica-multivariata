2c4ba5dff854e24723b7a422469022d881d96b83
fcosta00 <francescosta00@gmail.com>
2023-10-29 17:24:03 +0100

    parz esercizio individuale 2

---
+++ Esercizi/Individuale_2.R
@@ -0,0 +1,29 @@
+#Un gruppo di ricerca sta studiando la relazione tra Disturbo da Deficit di Attenzione e Iperattivit? (DDAI, o ADHD) e abuso di sostanze.
+#Da un'analisi della letteratura, emerge che le persone affette da ADHD hanno una probabilit? pi? alta, rispetto a persone non affette da questo disturbo,
+#di presentare in et? adulta un Disturbo da Abuso di Sostanze. Il gruppo di ricerca vuole testare se la relazione tra i due disturbi (ADHD e Abuso di sostanze) ? mediata dal fatto di frequentare "cattive compagnie".
+#In altre parole, i ricercatori vogliono verificare se il motivo reale per il quale gli ADHD presentano pi? spesso dei controlli un disturbo da abuso di sostanze ? imputabile al fatto che
+#gli ADHD hanno una probabilit? maggiore, rispetto ai controlli, di entrare in "cattive compagnie" che li portano ad entrare in contatto con le sostanze stupefacenti.
+
+
+
+#Per testare questa ipotesi, vengono selezionati un campione di persone con ADHD e un campione di persone senza ADHD (controlli).
+#Ciascun partecipante deve riportare se, in adolescenza, ha frequentato o meno "cattive compagnie".
+#Per ciascun soggetto viene misurata la gravit? dei comportamenti legati ad abuso di sostanze, attraverso una misura su scala ordinale da 1 a 100 (denominata Score).
+#I dati della ricerca sono contenuti nel database ADHD_sostanze.xlsx, scaricabile da Moodle.
+
+
+
+#Eseguire le seguenti operazioni (ricordarsi di convertire in variabili categoriali Gruppo e Cattive_compagnie):
+
+
+
+
+#Costruire una tavola di frequenza per le variabili Gruppo e Cattive_compagnie (considerate assieme).
+
+#Utilizzando istogrammi (in ggplot2), valutare e commentare la forma della distribuzione di Score, per ciascuna delle quattro combinazioni delle variabili Gruppo e Cattive_compagnie.
+#Eventualmente, utilizzare la funzione facet.grid() per rappresentare tutti e quattro i grafici "in un colpo solo".
+
+#Valutare la forma della distribuzione di Score, per ciascuna delle quattro combinazioni delle variabili Gruppo e Cattive_compagnie,
+#utilizzando sia i test numerici che il test di Shapiro-Wilk. Eventualmente, utilizzare la funzione by(), o in combinazione con la funzione stat.desc(), oppure da sola (come preferite).
+
+#Costruite un grafico (con ggplot2) per rappresentare i dati. Scegliete il tipo di grafico che preferite con le impostazioni grafiche che preferite. 
